4219cbf4ef3d9b7ebeba5f16a5075d9706391b9d
Anthony Wu <pls-file-gh-issue@users.noreply.github.com>
2025-07-11 00:03:22 -0700

    new default resources

---
+++ frontend/src/defaults.js
@@ -0,0 +1,7 @@
+export const Defaults = {
+  searchExamples: [
+    { emoji: "🪗", text: "Are accordians French?" },
+    { emoji: "🚀", text: "Who are the Guardians of the Galaxy?" },
+    { emoji: "⚽", text: "Who are the best soccer players of all time" },
+  ],
+};
