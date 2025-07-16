0561bbe1f818c81a590b27629ccac6b9fb3333a1
Anthony Wu <pls-file-gh-issue@users.noreply.github.com>
2025-07-16 10:39:01 -0700

    Fix UX paper cuts

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const Constants = {
   REACT_APP_API_URL: process.env.REACT_APP_API_URL,
   GITHUB_LINK:
-    process.env.GITHUB_LINK || "https://github.com/philfung/perplexed",
+    process.env.REACT_APP_GITHUB_LINK || "https://github.com/philfung/perplexed",
   JSON_STREAM_SEPARATOR: "[/PERPLEXED-SEPARATOR]",
 };
